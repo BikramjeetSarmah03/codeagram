8abc2b0a4d5c01f7973de2cf42be60fd76a29b7e
cognitive <dev.bikramjeet@gmail.com>
2024-01-13 14:35:36 +0530

    Prisma

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
+router.get("/test", (req: Request, res: Response) => {
   res.send("Api Working");
 });
 
